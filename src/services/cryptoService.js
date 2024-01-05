a5430050a581578ddcfdee4a71582dfd1143d6f2
FLEMIN <flemin.louies@unilogcorp.com>
2024-01-05 10:11:59 +0530

    key

--- src/services/cryptoService.js
+++ src/services/cryptoService.js
@@ -2,10 +2,11 @@ const axios = require('axios');
 require('dotenv').config();
 
 const API_BASE_URL = process.env.API_BASE_URL;
+const API_KEY = process.env.API_KEY;
 
 exports.getCrypto = async (pageSize, page) => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/coins/markets`, {
+    const response = await axios.get(`${API_BASE_URL}/coins/markets?x_cg_demo_api_key=${API_KEY}`, {
       params: {
         vs_currency: 'usd',
         order: 'market_cap_desc',
@@ -28,7 +29,7 @@ exports.getCrypto = async (pageSize, page) => {
 
 exports.getCurrency = async () => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/simple/supported_vs_currencies`);
+    const response = await axios.get(`${API_BASE_URL}/simple/supported_vs_currencies?x_cg_demo_api_key=${API_KEY}`);
     return response.data.map(currency => ({ name: currency.toUpperCase(), key: currency }));
   } catch (error) {
     // Create an error object with details
@@ -43,7 +44,7 @@ exports.getCurrency = async () => {
 
 exports.convertCurrency = async (sourceCrypto, amount, targetCurrency) => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/simple/price`, {
+    const response = await axios.get(`${API_BASE_URL}/simple/price?x_cg_demo_api_key=${API_KEY}`, {
       params: {
         ids: sourceCrypto,
         vs_currencies: targetCurrency,
